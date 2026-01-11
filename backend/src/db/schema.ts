0bf0ba408c93deaf45bb0783072f406b3ad452cb
Prem Sidapara <prem.sidapara.main@gmail.com>
2026-01-11 14:06:35 +0530

    add onUpdate trigger for updatedAt field in users table

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -9,7 +9,7 @@ export const users = pgTable("users", {
     name: text("name"),
     imageUrl: text("text_url"),
     createdAt: timestamp("created_at", {mode: "date"}).notNull().defaultNow(),
-    updatedAt: timestamp("updated_at", {mode: "date"}).notNull().defaultNow(),
+    updatedAt: timestamp("updated_at", {mode: "date"}).notNull().defaultNow().$onUpdate(()=> new Date()),
 })
 
 export const products = pgTable("products", {
